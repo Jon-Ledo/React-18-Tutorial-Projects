d6a2fc1edcb199e17d9c2fdc57f2e6bd9f1dac02
JonLedo <jonledo.code@gmail.com>
2023-04-06 09:33:58 -0400

    refactored the navbar to render links using map method. Created a data.js file to contain the info

---
+++ 00-backroads-landing-page/setup/src/data.js
@@ -0,0 +1,40 @@
+export const pageLinks = [
+  {
+    id: 1,
+    href: '#home',
+    text: 'home',
+  },
+  {
+    id: 2,
+    href: '#about',
+    text: 'about',
+  },
+  {
+    id: 3,
+    href: '#services',
+    text: 'services',
+  },
+  {
+    id: 4,
+    href: '#tours',
+    text: 'tours',
+  },
+]
+
+export const socialsLinks = [
+  {
+    id: 1,
+    icon: 'fab fa-facebook',
+    href: 'https://twitter.com/',
+  },
+  {
+    id: 2,
+    icon: 'fab fa-twitter',
+    href: 'https://twitter.com/',
+  },
+  {
+    id: 3,
+    icon: 'fab fa-squarespace',
+    href: 'https://twitter.com/',
+  },
+]
