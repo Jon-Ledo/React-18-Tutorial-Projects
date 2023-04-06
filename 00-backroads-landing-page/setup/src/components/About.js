d95e5d8b1a764d82ed7b0cde68a3625a4cfc67ce
JonLedo <jonledo.code@gmail.com>
2023-04-06 10:43:07 -0400

    refactored code to remove duplicates. Set up more data to iterate over

--- 00-backroads-landing-page/setup/src/components/About.js
+++ 00-backroads-landing-page/setup/src/components/About.js
@@ -1,13 +1,10 @@
 import aboutImg from '../images/about.jpeg'
+import Title from './Title'
 
 const About = () => {
   return (
     <section className='section' id='about'>
-      <div className='section-title'>
-        <h2>
-          about <span>us</span>
-        </h2>
-      </div>
+      <Title title='about' span='us' />
 
       <div className='section-center about-center'>
         <div className='about-img'>
--- 00-backroads-landing-page/setup/src/data.js
+++ 00-backroads-landing-page/setup/src/data.js
@@ -1,3 +1,8 @@
+import tour1 from './images/tour-1.jpeg'
+import tour2 from './images/tour-2.jpeg'
+import tour3 from './images/tour-3.jpeg'
+import tour4 from './images/tour-4.jpeg'
+
 export const pageLinks = [
   {
     id: 1,
@@ -36,5 +41,69 @@ export const socialsLinks = [
     id: 3,
     icon: 'fab fa-squarespace',
     href: 'https://twitter.com/',
+  },
+]
+
+export const services = [
+  {
+    id: 1,
+    icon: 'fas fa-wallet fa-fw',
+    title: 'saving money',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.',
+  },
+  {
+    id: 2,
+    icon: 'fas fa-tree fa-fw',
+    title: 'endless hiking',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.',
+  },
+  {
+    id: 3,
+    icon: 'fas fa-socks fa-fw',
+    title: 'amazing comfort',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.',
+  },
+]
+
+export const tours = [
+  {
+    id: 1,
+    title: 'Tibet Adventure',
+    image: tour1,
+    date: 'august 26th, 2020',
+    location: 'china',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    duration: 6,
+    cost: 2100,
+  },
+  {
+    id: 2,
+    title: 'best of java',
+    image: tour2,
+    date: 'october 1th, 2020',
+    location: 'indonesia',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    duration: 11,
+    cost: 1400,
+  },
+  {
+    id: 3,
+    title: 'explore hong kong',
+    image: tour3,
+    date: 'september 15th, 2020',
+    location: ' hong kong',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    duration: 8,
+    cost: 5000,
+  },
+  {
+    id: 1,
+    title: 'kenya highlights',
+    image: tour4,
+    date: 'december 5th, 2019',
+    location: 'kenya',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    duration: 20,
+    cost: 3300,
   },
 ]
